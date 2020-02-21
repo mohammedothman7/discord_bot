ff63ac1af95231f358894240e100343bdc36a068
BuildTools <unconfigured@null.spigotmc.org>
2020-02-21 16:24:36 -0500

    Added music_commands, cleaned up bot.py, changned how queue command works

--- basic_events.py
+++ basic_events.py
@@ -5,7 +5,10 @@ from discord.ext import commands
 from discord.utils import get
 
 
+
 class BasicEvents(commands.Cog):
+    default_role = "Member"
+
     def __init__(self, bot):
         self.bot = bot
 
---
+++ music_commands.py
@@ -0,0 +1,251 @@
+# Music commands
+import os
+import shutil
+
+import discord
+from discord.ext import commands
+from discord.utils import get
+import youtube_dl
+
+queues = {}
+global is_bot_connected_to_voice
+
+class MusicCommands(commands.Cog):
+
+
+    @commands.command(aliases=["j", "joi"])
+    async def join(self, ctx):
+        global voice
+
+        channel = ctx.message.author.voice.channel
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
+            print(f"The bot has connected to {channel}\n")
+            is_bot_connected_to_voice = True
+            print(is_bot_connected_to_voice)
+            await ctx.send(f"Joined {channel}")
+
+    @commands.command()
+    async def leave(self, ctx):
+        channel = ctx.message.author.voice.channel
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+
+        if voice and voice.is_connected():
+            await voice.disconnect()
+            print(f"The bot has left the {channel} channel")
+            await ctx.send(f"Left {channel}")
+            is_bot_connected_to_voice = False
+        else:
+            print(f"Not in a voice channel")
+            await ctx.send("Not in a voice channel")
+
+    @commands.command()
+    async def play(self, ctx, url: str):
+        print(f"Bot connected? {is_bot_connected_to_voice}")
+        if not is_bot_connected_to_voice:
+            await MusicCommands.join(ctx)
+
+        def check_queue():
+            Queue_infile = os.path.isdir("./Queue")
+            if Queue_infile is True:
+                DIR = os.path.abspath(os.path.realpath("Queue"))
+                length = len(os.listdir(DIR))
+                still_q = length - 1
+                try:
+                    first_file = os.listdir(DIR)[0]
+                except:
+                    print("No more queued song(s)\n")
+                    queues.clear()
+                    return
+                main_location = os.path.dirname(os.path.realpath(__file__))
+                song_path = os.path.abspath(os.path.realpath("Queue") + "\\" + first_file)
+                if length != 0:
+                    print("Song done, playing next queued\n")
+                    print(f"Songs still in queue: {still_q}")
+                    song_there = os.path.isfile("song.mp3")
+                    if song_there:
+                        os.remove("song.mp3")
+                    shutil.move(song_path, main_location)
+                    for file in os.listdir("./"):
+                        if file.endswith(".mp3"):
+                            os.rename(file, 'song.mp3')
+
+                    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
+                    voice.source = discord.PCMVolumeTransformer(voice.source)
+                    voice.source.volume = 0.07
+
+                else:
+                    queues.clear()
+                    return
+
+            else:
+                queues.clear()
+                print("No songs were queued before the ending of the last song\n")
+
+        song_there = os.path.isfile("song.mp3")
+
+        try:
+            if song_there:
+                os.remove("song.mp3")
+                queues.clear()
+                print("Removed old song file")
+        except PermissionError:
+            await MusicCommands.queue(ctx, url)
+            print("Trying to delete song file, but it's being played")
+            await ctx.send("Music playing, use -queue to add it to the playlist.")
+
+            return
+
+        Queue_infile = os.path.isdir("./Queue")
+        try:
+            Queue_folder = "./Queue"
+            if Queue_infile is True:
+                print("Removed old Queue Folder")
+                shutil.rmtree(Queue_folder)
+        except:
+            print("No old Queue folder")
+
+        await ctx.send("Getting everything ready now")
+
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'quiet': True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            print("Downloading audio now\n")
+            ydl.download([url])
+
+        for file in os.listdir("./"):
+            if file.endswith(".mp3"):
+                name = file
+                print(f"Renamed File: {file}\n")
+                os.rename(file, "song.mp3")
+
+        voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
+        voice.source = discord.PCMVolumeTransformer(voice.source)
+        voice.source.volume = 0.07
+
+        song_name = name.rsplit("-", 2)
+        await ctx.send(f"Playing: {song_name[0]}")
+        print("playing\n")
+
+    queues = {}
+
+    @commands.command(pass_context=True, aliases=['pa', 'pau'])
+    async def pause(self, ctx):
+
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+
+        if voice and voice.is_playing():
+            print("Music paused")
+            voice.pause()
+            await ctx.send("Music paused")
+        else:
+            print("Music not playing failed pause")
+            await ctx.send("Music not playing failed pause")
+
+    @commands.command(pass_context=True, aliases=['r', 'res'])
+    async def resume(self, ctx):
+
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+
+        if voice and voice.is_paused():
+            print("Resumed music")
+            voice.resume()
+            await ctx.send("Resumed music")
+        else:
+            print("Music is not paused")
+            await ctx.send("Music is not paused")
+
+    @commands.command(pass_context=True, aliases=['s', 'sto'])
+    async def stop(self, ctx):
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+
+        queues.clear()
+
+        if voice and voice.is_playing():
+            print("Music stopped")
+            voice.stop()
+            await ctx.send("Music stopped")
+        else:
+            print("No music playing failed to stop")
+            await ctx.send("No music playing failed to stop")
+
+
+
+    @commands.command(pass_context=True, aliases=['q', 'que'])
+    async def queue(self, ctx, url: str):
+        Queue_infile = os.path.isdir("./Queue")
+        if Queue_infile is False:
+            os.mkdir("Queue")
+        DIR = os.path.abspath(os.path.realpath("Queue"))
+        q_num = len(os.listdir(DIR))
+        q_num += 1
+        add_queue = True
+        while add_queue:
+            if q_num in queues:
+                q_num += 1
+            else:
+                add_queue = False
+                queues[q_num] = q_num
+
+        queue_path = os.path.abspath(os.path.realpath("Queue") + f"\song{q_num}.%(ext)s")
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'quiet': True,
+            'outtmpl': queue_path,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            print("Downloading audio now\n")
+            ydl.download([url])
+        await ctx.send(f"Added song. {q_num} in queue.")
+
+        print("Song added to queue\n")
+
+    @commands.command(aliases=["vol", "v", "vl"])
+    async def volume(self, ctx, vol: int = 5):
+        global volume
+
+        if vol < 1:
+            await ctx.send("The volume you chose is too low, please pick a number between 1 and 10")
+        elif vol > 10:
+            await ctx.send("The volume you chose is too high, please pick a number between 1 and 10")
+        else:
+            volume = vol / 25
+            voice.source.volume = volume
+            print(f"Volume changed to {vol}")
+            await ctx.send(f"Changed the volume to {vol}")
+
+    @commands.command(aliases=["next", "ski", "sk", "ne"])
+    async def skip(self, ctx):
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+
+        if voice and voice.is_playing():
+            print("Playing Next Song")
+            voice.stop()
+            await ctx.send("Next Song")
+        else:
+            print("No music playing")
+            await ctx.send("No music playing failed")
+
+def setup(bot):
+    bot.add_cog(MusicCommands(bot))

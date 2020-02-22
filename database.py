b79746fc82b835ca296898734263f6a9611d3361
BuildTools <unconfigured@null.spigotmc.org>
2020-02-22 14:58:03 -0500

    Changed database and collection name to fit better

--- database.py
+++ database.py
@@ -3,6 +3,9 @@
 import pymongo
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://moe:123@discord-bot-zsgs1.mongodb.net/test?retryWrites=true&w=majority")
-db = client["test"]
-collection = db["test"]
+try:
+    client = MongoClient("mongodb+srv://moe:123@discord-bot-zsgs1.mongodb.net/test?retryWrites=true&w=majority")
+    db = client["discord"]
+    collection = db["users"]
+except Exception:
+    print("Failed to connect to database")

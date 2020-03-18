eb6bd5a050e4e5c61f97668cff17853e0ac72277
BuildTools <unconfigured@null.spigotmc.org>
2020-03-18 03:42:58 -0400

    Added auto task that checks if users should be unbanned or unmuted based on if their punishment time is over. Also logs to db. Added banned_users and muted_users db.

--- bot_tasks.py
+++ bot_tasks.py
@@ -1,27 +1,50 @@
 import discord
 from discord.ext import commands, tasks
 from database import db
-import datetime
+from datetime import datetime
 
-banned_users = db["banned_users"]
+banned_users_db = db["banned_users"]
+logs_db = db['logs']
+muted_users_db = db['muted_users']
+
 
 class BotTasks(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.auto.start()
 
+    @tasks.loop(minutes=10)
+    async def auto(self):
+        print(f"Running task")
+        now = datetime.utcnow()
+        guild = self.bot.get_guild(677957812965343262)
 
-    @tasks.loop(seconds=15)
-    async def auto_unban(self):
-        now = datetime.datetime.utcnow()
+        for user in banned_users_db.find():  # Iterating through the banned users db
+            ban_time = user['BANNED TIL']
+            user_name = user['NAME']
+            user_id = user['_id']
+            if now >= ban_time:  # Checking if the USERS banned time is up
+                await guild.unban(discord.Object(id=user_id))
+                print(f"Auto Unbanned: {user_name}")
 
-        for user in banned_users.find():
-            ban_time = banned_users.find_one({"banned til"})
-            print(ban_time)
+        for user in muted_users_db.find():  # Iterating through the muted users db
+            muted_time = user['MUTED TIL']
+            muted_user_name = user['NAME']
+            muted_user_id = user['_id']
+            member = guild.get_member(muted_user_id)
 
-            if ban_time >= now:
-                return
+            if now >= muted_time:  # Checking if the USERS banned time is up
+                await member.remove_roles(discord.utils.get(guild.roles, name="Muted"))
+                print(f"Auto Unmuted: {muted_user_name}")
+                muted_users_db.remove({'_id': muted_user_id})
+                post = {"USER ID": user.id, "TYPE:": 'MUTED', "NAME": user.name + '#' + user.discriminator,
+                        "UNMUTED AT": datetime.utcnow()}
+                logs_db.insert_one(post)
 
-
+    @auto.before_loop
+    async def before_auto_unban(self):
+        print('Waiting for bot to be ready to start loop')
+        await self.bot.wait_until_ready()
 
 
 def setup(bot):
